4502d3abd57e2f6dd094b56896508a8e4d5de7e9
ryaaan4321 <aryan056400@gmail.com>
2026-01-03 03:02:52 +0530

    first commit

---
+++ app/js/page.js
@@ -0,0 +1,15 @@
+"use client"
+
+import { useEffect } from "react";
+
+
+export default function Page() {
+    return (
+        <div>
+            <button
+                className="bg-gray rounded-e-2xl rounded-b-2xl text-white cursor-pointer p-1 bg-green-400 ml-4 mt-4"
+            // onClick={}
+            >Fuck me Daddy..! </button>
+        </div>
+    )
+}
